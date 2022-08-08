a8e859b2d3a6b9c595258f66970ec5b9c8414257
MrValioBg <valentin.v1102@gmail.com>
2022-08-08 16:59:06 +0300

    Initial commit

---
+++ src/com/codewithmosh/memento_done/History.java
@@ -0,0 +1,18 @@
+package com.codewithmosh.memento_done;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class History {
+
+    private final Deque<DocumentState> states = new ArrayDeque<>();
+
+    public void push(DocumentState state) {
+        states.push(state);
+    }
+
+    public DocumentState pop() {
+        return states.pop();
+    }
+
+}
---
+++ src/com/codewithmosh/state/travelmodes/TravelMode.java
@@ -0,0 +1,7 @@
+package com.codewithmosh.state.travelmodes;
+
+public interface TravelMode {
+
+    Object getEta();
+    Object getDirection();
+}
